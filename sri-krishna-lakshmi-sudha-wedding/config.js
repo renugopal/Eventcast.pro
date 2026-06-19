39873fa9a9773631153d0e120516f52bcd13e2d4
renugopal <renugopalchebrolu@gmail.com>
2026-06-19 06:37:53 +0530

    feat: Add Sri Krishna and Lakshmi Sudha wedding livestream event template

---
+++ sri-krishna-lakshmi-sudha-wedding/config.js
@@ -0,0 +1,26 @@
+window.WEDDING_CONFIG = {
+    groom: "Sri Krishna",
+    bride: "Lakshmi Sudha",
+    date: "Friday, June 19",
+    time: "09:14 PM",
+    timeSubtext: "Muhurtham",
+    timerTarget: "2026-06-19T19:00:00",
+    venue: "Sri Sahasra Lingeswara Swami Kalyana Mandapam",
+    venueSubtext: "Ponnur",
+    youtubeId: "XkIYrYL4QhY",
+    invitationVideo: "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808274739-Cards_Invitation_.mp4",
+    thumbnail: "assets/thumb.jpeg",
+    gallery: [
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808291564-WhatsApp_Image_2026-06-17_at_2.24.49_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808292169-WhatsApp_Image_2026-06-17_at_2.24.50_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808292787-WhatsApp_Image_2026-06-17_at_2.24.52_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808293314-WhatsApp_Image_2026-06-17_at_2.24.55_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808293819-WhatsApp_Image_2026-06-17_at_2.25.04_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808294372-WhatsApp_Image_2026-06-17_at_2.25.08_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808294906-WhatsApp_Image_2026-06-17_at_2.25.11_PM.jpeg",
+        "https://pub-fa013cc979d8410e9d307bd2c9e6ecf2.r2.dev/events/eventcast/1781808295509-WhatsApp_Image_2026-06-17_at_2.25.13_PM.jpeg"
+    ],
+    supabaseUrl: 'https://lteogzqeuuoxlekhofow.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0ZW9nenFldXVveGxla2hvZm93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxOTc0ODIsImV4cCI6MjA5Mjc3MzQ4Mn0.eaGIKI5O17PI-bh52IiDDpPr4vqlBOU1Bq_NuAkYZXE',
+    eventId: 'sri-krishna-lakshmi-sudha'
+};
